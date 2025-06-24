d059ac062ee7f8d751b4b357f5577092eaed65b5
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-06-24 09:49:39 +0530

    Create Python Program to Find the Sum of First N Natural Numbers.py

---
+++ Basic Python Code/Python Program to Find the Sum of First N Natural Numbers.py
@@ -0,0 +1,24 @@
+ ---- Using for Loop ----
+num = int(input())
+sum = 0
+for i in range(num+1):
+  sum+=i
+print(sum)
+
+-----Using Formula for the Sum of Nth Term-----
+num = int(input())
+print(int(num*(num+1)/2))
+
+---- Using Recursion ----
+def getSum(num):
+  if num == 1:
+    return 1
+  return num + getSum(num-1)
+
+num = int(input())
+print(getSum(num))
+
+
+---OUTPUT: ----
+8
+36
