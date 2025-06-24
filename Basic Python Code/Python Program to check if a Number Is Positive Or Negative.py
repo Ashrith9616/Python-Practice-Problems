01af115b58d3a4e4aa12a1539a5cb935a4722bf1
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-06-24 09:47:58 +0530

    Create Python Program to check if a Number Is Positive Or Negative.py

---
+++ Basic Python Code/Python Program to check if a Number Is Positive Or Negative.py
@@ -0,0 +1,28 @@
+--- USING BRUTE FORCE ----
+num = int(input())
+if num > 0:
+    print('Positive')
+elif num < 0:
+    print('Negative')
+else:
+    print('Zero')
+
+---- USING Nested if-else Statements ----
+num = int(input())
+if num>=0:
+    if num==0:
+        print('Zero')
+    else:
+        print("Positive")
+else:
+    print("Negative")
+
+---- USING TERNARY OPERATORS -------
+num = int(input())
+print("Positive" if num>=0 else "Negative")
+
+---output: -----
+8 
+positive
+-9
+negative
