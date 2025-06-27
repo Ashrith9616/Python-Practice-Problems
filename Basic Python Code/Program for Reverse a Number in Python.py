ceb39414221fadee35397516985bb8c150d5fb6e
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-06-27 22:13:52 +0530

    Create Program for Reverse a Number in Python.py

---
+++ Basic Python Code/Program for Reverse a Number in Python.py
@@ -0,0 +1,14 @@
+----  Using Simple Iteration -----
+num = int(input())
+temp = num
+reverse = 0
+while num > 0:
+    remainder = num % 10
+    reverse = (reverse * 10) + remainder
+    num = num // 10
+
+print(reverse)
+
+---- Output ----
+12345
+:= 54321
