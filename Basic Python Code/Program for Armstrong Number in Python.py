d853e4be34050905595d9e1a0c19ef7d4ffbd70b
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-06-27 22:26:23 +0530

    Create Program for Armstrong Number in Python.py

---
+++ Basic Python Code/Program for Armstrong Number in Python.py
@@ -0,0 +1,19 @@
+---- Using Iteration ----
+number = int(input())
+num = number
+digit, sum = 0, 0
+length = len(str(num))
+for i in range(length):
+  digit = int(num%10)
+  num = num/10
+  sum += pow(digit,length)
+if sum==number:
+  print("Armstrong")
+else:
+  print("Not Armstrong")
+
+---- Output ----
+370
+:=Armstrong
+123
+:=Not Armstrong
