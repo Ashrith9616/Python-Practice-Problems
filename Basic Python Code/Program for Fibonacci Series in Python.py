24b2c53cba485114ca7e90133b3b04a662ca2804
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-06-28 22:01:21 +0530

    Create Program for Fibonacci Series in Python.py

---
+++ Basic Python Code/Program for Fibonacci Series in Python.py
@@ -0,0 +1,15 @@
+----  Using Simple Iteration ----
+num = int(input())
+n1, n2 = 0, 1
+print("Fibonacci Series:", n1, n2, end=" ")
+for i in range(2, num):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3, end=" ")
+
+print()
+
+---- Output ----
+10  
+Fibonacci Series: 0 1 1 2 3 5 8 13 21 34 
