5d6dbdfc986d1570e19232ba0573bf7d9c364ca3
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-06-30 21:29:21 +0530

    Create Python program to find factors of a number.py

---
+++ Basic Python Code/Python program to find factors of a number.py
@@ -0,0 +1,12 @@
+---- Using (1,num+1) as Range ----
+
+num=int(input())
+factor=[]
+for i in range(1,num+1):
+    if (num%i==0):
+        factor.append(i)
+print(factor)
+
+---- Output ----
+15
+:=[1, 3, 5, 15]
