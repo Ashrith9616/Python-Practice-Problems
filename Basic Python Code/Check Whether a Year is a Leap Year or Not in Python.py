07299389e9677479335900a1ca436d9889258017
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-07-01 22:33:20 +0530

    Update Check Whether a Year is a Leap Year or Not in Python.py

--- Basic Python Code/Check Whether a Year is a Leap Year or Not in Python.py
+++ Basic Python Code/Check Whether a Year is a Leap Year or Not in Python.py
@@ -1,4 +1,4 @@
----- Using if-else statements (1) -----
+---- Using if-else statements (1) ----
 year = int(input())
 if (year%400 == 0) or (year%4==0 and year%100!=0):
     print("Leap Year")
@@ -12,7 +12,7 @@ if( ((year % 4 == 0) and (year % 100 != 0)) or (year % 400==0) ):
 else:
     print("Not a leap Year")
 
----- Output ----
+---- Output: ----
 2024
 := Leap Year
 2018
