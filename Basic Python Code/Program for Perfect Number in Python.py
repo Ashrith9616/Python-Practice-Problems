307a86f3d13c64a1c893f3f1e21939273fe58a0d
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-07-02 23:34:51 +0530

    Create Program for Perfect Number in Python.py

---
+++ Basic Python Code/Program for Perfect Number in Python.py
@@ -0,0 +1,19 @@
+---- Using For Loop Iteration between [1, num] ----
+
+n = int(input())
+sum = 0
+
+for i in range(1, n):
+    if n % i == 0:
+        sum = sum + i
+
+if sum == n:
+    print("The number is a Perfect number")
+else:
+    print("The number is not a Perfect number")
+
+---- Output ----
+28
+:= The number is a Perfect number
+10
+:= The number is not a Perfect number
