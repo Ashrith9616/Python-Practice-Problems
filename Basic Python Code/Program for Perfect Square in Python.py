ff2294458b9dadffd014523490e7888d25400150
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-07-02 23:42:45 +0530

    Program for Perfect Square in Python.py

---
+++ Basic Python Code/Program for Perfect Square in Python.py
@@ -0,0 +1,20 @@
+---- Using Simple iteration ----
+
+import math
+num = int(input("Enter a number: "))
+x = math.sqrt(num)
+y = int(x)
+a = y * y
+
+if num == a:
+    print("perfect square")
+else:
+    print("not a perfect square")
+
+---- Output ----
+
+Enter a number: 25
+:= perfect square
+
+Enter a number: 10
+:= not a perfect square
