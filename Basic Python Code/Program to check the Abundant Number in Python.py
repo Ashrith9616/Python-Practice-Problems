104de1776c6e0c4a211b57a82aca3e12e0d099af
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-07-03 22:39:12 +0530

    Create Program to check the Abundant Number in Python.py

---
+++ Basic Python Code/Program to check the Abundant Number in Python.py
@@ -0,0 +1,20 @@
+---- Using Range until Number ----
+
+n = int(input("Enter a number: "))
+sum = 1 
+
+for i in range(2,n):
+  if(n%i==0):  
+        sum=sum+i
+
+if(sum>n):
+  print(n,'is Abundant Number')
+else:
+  print(n,'is not Abundant Number')
+
+---- Output ----
+Enter a number: 12
+:= 12 is Abundant Number
+
+Enter a number: 15
+:= 15 is not Abundant Number
