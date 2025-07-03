5cf568c1dfee8207c149930e16292728b3ea3340
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-07-03 22:29:15 +0530

    Program to Check the Number is Harshad or not using Python.py

---
+++ Basic Python Code/Program to Check the Number is Harshad or not using Python.py
@@ -0,0 +1,17 @@
+----  Using Iteration I ----
+
+N=input(“Enter a number:”)
+sum=0
+for i in N:
+    sum+=int(i)
+if int(N)%sum==0:
+    print("Harshad number")
+else:
+    print("Not a Harshad Number")
+
+---- Output ----
+Enter a number:18
+:= Harshad number
+
+Enter a number:25
+:= Not a Harshad Number
