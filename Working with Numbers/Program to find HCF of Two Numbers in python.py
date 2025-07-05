4778196b7fdda3ffee564ff1eacfed1968150bb7
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-07-05 09:59:10 +0530

    Program to find HCF of Two Numbers in python.py

---
+++ Working with Numbers/Program to find HCF of Two Numbers in python.py
@@ -0,0 +1,15 @@
+---- Linear Quest to find HCF ----
+
+num1 = int(input("Enter the Number: "))
+num2 = int(input("Enter the Number: "))
+hcf = 1
+
+for i in range(1, min(num1, num2)):
+    if num1 % i == 0 and num2 % i == 0:
+        hcf = i
+print("Hcf of", num1, "and", num2, "is", hcf)
+
+---- Output ----
+Enter the Number: 4
+Enter the Number: 4
+:= Hcf of 4 and 4 is 2
