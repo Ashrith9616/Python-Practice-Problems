f7e2cce42dfb10f0761d8bc436f5838e64178f4f
Ashrith9616 <ashrithchittaluru@gmail.com>
2025-07-09 23:01:05 +0530

    Program for Binary To Decimal Conversion in  python.py

---
+++ Working with Numbers/Program for Binary To Decimal Conversion in  python.py
@@ -0,0 +1,9 @@
+--- Using Simple iteration ----
+
+binary = input("Enter a binary number: ")
+decimal = int(binary, 2)
+print("Decimal value:", decimal)
+
+---- Output ----
+Enter a binary number: 1010
+Decimal value: 10
